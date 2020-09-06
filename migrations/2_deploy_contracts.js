6e13c791a2c9f1542ee06d2cd5dee59da1182fbe
Restyled.io <commits@restyled.io>
2020-09-06 12:50:40 +0000

    Restyled by clang-format

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,3 @@
 var Adoption = artifacts.require("Adoption");
 
-module.exports = function (deployer) {
-  deployer.deploy(Adoption);
-};
+module.exports = function(deployer) { deployer.deploy(Adoption); };
--- truffle-config-besu.js
+++ truffle-config-besu.js
@@ -4,23 +4,25 @@ require("babel-polyfill");
 const PrivateKeyProvider = require("@truffle/hdwallet-provider");
 const privateKey = "<account-private-key>";
 const privateKeyProvider = new PrivateKeyProvider(privateKey, "127.0.0.1:8545");
-// @dev privateKeyProvider running locally, this means your running a node on your computer ethsigner
+// @dev privateKeyProvider running locally, this means your running a node on
+// your computer ethsigner
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // for more about customizing your Truffle configuration!
-  networks: {
-    besuWallet: {
-      provider: privateKeyProvider,
-      host: "<IP_ADDRESS>",
-      port: 8545,
-      network_id: "*",
+  networks : {
+    besuWallet : {
+      provider : privateKeyProvider,
+      host : "<IP_ADDRESS>",
+      port : 8545,
+      network_id : "*",
     },
   },
-  // https://www.trufflesuite.com/docs/truffle/reference/configuration#compiler-configuration for different versions of solidity ^0.5 >
-  compilers: {
-    solc: {
-      version: "0.5.16", //  solc-js
+  // https://www.trufflesuite.com/docs/truffle/reference/configuration#compiler-configuration
+  // for different versions of solidity ^0.5 >
+  compilers : {
+    solc : {
+      version : "0.5.16", //  solc-js
     },
   },
 };

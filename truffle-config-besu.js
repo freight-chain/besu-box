c1cd80b162c5542e319fc23b9ecac8200cd0ed32
Restyled.io <commits@restyled.io>
2020-09-06 12:50:58 +0000

    Restyled by prettier

--- truffle-config-besu.js
+++ truffle-config-besu.js
@@ -10,19 +10,19 @@ const privateKeyProvider = new PrivateKeyProvider(privateKey, "127.0.0.1:8545");
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // for more about customizing your Truffle configuration!
-  networks : {
-    besuWallet : {
-      provider : privateKeyProvider,
-      host : "<IP_ADDRESS>",
-      port : 8545,
-      network_id : "*",
+  networks: {
+    besuWallet: {
+      provider: privateKeyProvider,
+      host: "<IP_ADDRESS>",
+      port: 8545,
+      network_id: "*",
     },
   },
   // https://www.trufflesuite.com/docs/truffle/reference/configuration#compiler-configuration
   // for different versions of solidity ^0.5 >
-  compilers : {
-    solc : {
-      version : "0.5.16", //  solc-js
+  compilers: {
+    solc: {
+      version: "0.5.16", //  solc-js
     },
   },
 };
